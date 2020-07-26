056a582e9274e68bda9aef23f7816118956adc2e
pedroprado_bexs <pedro.prado@bexsbanco.com.br>
2020-07-26 02:11:03 -0300

    Added asyncronous function: rendering only after the async call is resolved

--- learning-nextjs/pages/auth/index.js
+++ learning-nextjs/pages/auth/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
 
-const authIndexPage = () =>{
-   return ( 
-        <div>
-            <h1>The Authentication Page!</h1>
-        </div>
-    );
+import User from "../../components/User";
+
+const authIndexPage = (props) => (
+  <div>
+    <h1>The Auth Index Page - {props.appName}</h1>
+    <User name="Max" age={28} />
+  </div>
+);
+
+authIndexPage.getInitialProps = context => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ appName: "Super App (Auth)" });
+    }, 1000);
+  });
+  return promise;
 };
 
 export default authIndexPage;
